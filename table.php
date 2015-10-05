72970bc533c53ff9fbf456c05b442c44e4967645
Merit Paist <meripai@greeny.cs.tlu.ee>
2015-10-05 15:05:54 +0300

    täiendusi

--- table.php
+++ table.php
@@ -17,18 +17,33 @@ require_once("functions.php");
 	<th>Number</th>
 	<th>Värv</th>
 	<th>Kustuta</th>
+	<th>Edit</th>
 </tr>  
  <?php
  
       //autod ükshaaval läbi käia
     for($i = 0; $i < count($car_array); $i++){
+		
+		//kasutaja tahab rida muuta
+	if(isset($_GET["edit"])&& $_GET["edit"]==$car_array[$i]->id){
+		echo "<tr>";
+		echo "<td>".$car_array[$i]->id."</td>";  
+        echo "<td>".$car_array[$i]->user_id."</td>";  
+	    echo "<td><input name='number_plate' value='".$car_array[$i]->number_plate."'</td>";  
+		echo "<td><input name='color' value='".$car_array[$i]->color."'</td>"; 
+		echo "<td><a href='?table.php=".$car_array[$i]->id."'>Katkesta</a></td>";  ///siia jäi mingi jama sisse
+	    echo "<td><a href='?update=".$car_array[$i]->id."'>Salvesta</a></td>";  
+		echo"</tr>";
+	
+		}else{
     echo "<td>".$car_array[$i]->id."</td>";  
     echo "<td>".$car_array[$i]->user_id."</td>";  
 	echo "<td>".$car_array[$i]->number_plate."</td>";  
 	echo "<td>".$car_array[$i]->color."</td>"; 
 	echo "<td><a href='?delete=".$car_array[$i]->id."'>X</a></td>";  
+	echo "<td><a href='?edit=".$car_array[$i]->id."'>E</a></td>";  
     echo "</tr>"; 
-	
+	}
    }
 	
   
