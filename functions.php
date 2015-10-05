4b4bf64d3d730131d480b651180403672c2ced28
Merit Paist <meripai@greeny.cs.tlu.ee>
2015-10-05 21:22:45 +0300

    mõned parandused

--- functions.php
+++ functions.php
@@ -23,9 +23,10 @@
 					// suvaline muutuja, kus hoiame auto andmeid massiivi lisamiseni
 					$car = new StdClass();
 				    $car-> id = $id_from_db;
-				    $car-> color = $color_from_db;
-				    $car-> user_id = $user_id_from_db;
-					$car-> number_plate = $number_plate_from_db;
+				    $car-> user_id = $user_id_from_db; 
+				    $car-> number_plate = $number_plate_from_db;
+					$car-> color = $color_from_db;
+					
 					//lisan massiivi
 					array_push($array, $car);
 					//echo "<pre>";
@@ -35,20 +36,22 @@
 				return $array;	
 				   
 					
-	       $stmt->close(); 
-		   $mysqli->close();      
+	          $stmt->close(); 
+		      $mysqli->close();      
 		   } 
+		   
   function deleteCarData($car_id){
-		     $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);  		
+	  
+	 	     $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);  		
              $stmt = $mysqli->prepare("UPDATE car_plates SET deleted=NOW() WHERE id=?");
 			 $stmt -> bind_param("i", $car_id);
 			 $stmt->execute();
 			 // tühjendame aadressirea
-			 header("location:table.php");
+			 header("location: table.php");
              $stmt->close(); 
 		     $mysqli->close();
-  function updateCarData($car_id, $number_plate, $color) {			 
-   $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]); 
+   function updateCarData($car_id, $number_plate, $color) {			 
+            $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]); 
    
              $stmt = $mysqli->prepare("UPDATE car_plates SET number_plate=?, color=? WHERE id=?");
 			 $stmt -> bind_param("ssi", $number_plate, $color, $car_id);
@@ -58,4 +61,6 @@
              $stmt->close(); 
 		     $mysqli->close();
      }   
+	 
+	 
 ?>
